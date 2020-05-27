6be8d50a2e1bebe5967f52783a00e4f9d70f73b7
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-05-27 22:25:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const session    = require("express-session");
+const MongoStore = require('connect-mongo')(session);
+const flash      = require("connect-flash");
+const cors       = require("cors");
+    
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((x) => {
+    `Connected to Mongo! Host: ${x.connections[0].host}, Port: ${x.connections[0].port}, Database name: "${x.connections[0].name}"`;
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://drumgrid.nl:3000'],
+  credentials: true
+}))
+
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+// Enable authentication using session + passport
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(flash());
+require('./passport')(app);
+    
+
+const index = require('./routes/index');
+app.use('/', index);
+
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
+      
+
+module.exports = app;
