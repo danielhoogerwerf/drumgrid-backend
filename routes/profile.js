5a4dbcc1937036fe333ea0ff01656357f056b672
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-06-18 22:51:11 +0200

    made updateprofile

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 // Models
@@ -8,46 +9,37 @@ const Profile = require("../models/Profile");
 
 // Routes
 
-// POST - changeusername
-router.post("/changeusername", (req, res) => {
-  if (!req.isAuthenticated()) {
-    res.status(403).json({ message: "please authenticate" });
-    return;
-  }
-
-  const username = req.body.username;
-  User.findOne({ username }).then((user) => {
-    if (user !== null) {
-      return res.status(409).json({ message: "The username already exists" });
-    }
-
-    User.updateOne({ _id: req.user.id }, { username })
-      .then((result) => res.status(200).json(result))
-      .catch((e) => res.status(500).json({ error: `an error occured: ${e}` }));
-  });
-});
-
-// POST - changepassword
-router.post("/changepassword", (req, res) => {
+// POST - updateprofile
+router.post("/updateprofile", (req, res) => {
   if (!req.isAuthenticated()) {
     return res.status(403).json({ message: "Please authenticate" });
   }
 
-  const password = req.body.password;
-  if (!password) {
-    return res.status(400).json({ message: "Password cannot be blank" });
+  if (!req.body.email) {
+    return res.status(400).json({ message: "Email cannot be blank" });
   }
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      User.updateOne({ _id: req.user.id }, { password: hash })
-        .then(() => {
-          res.status(200).json({ message: "Password updated" });
-        })
-        .catch((e) => res.status(500).json({ error: `an error occured: ${e}` }));
-    })
-    .catch((e) => res.status(500).json({ error: `an error occured: ${e}` }));
+  const email = req.body.email.toLowerCase().replace(/[^A-Za-z0-9_\-@\.!]+/g, "")
+  const password = req.body.password;
+
+  if (password) {
+    bcrypt
+      .hash(password, 10)
+      .then((hash) => {
+        User.updateOne({ _id: req.user.id }, { email, password: hash })
+          .then(() => {
+            res.status(200).json({ message: "Profile updated" });
+          })
+          .catch((e) => res.status(500).json({ error: `an error occured: ${e}` }));
+      })
+      .catch((e) => res.status(500).json({ error: `an error occured: ${e}` }));
+  } else {
+    User.updateOne({ _id: req.user.id }, { email })
+      .then(() => {
+        res.status(200).json({ message: "Email updated" });
+      })
+      .catch((e) => res.status(500).json({ error: `an error occured: ${e}` }));
+  }
 });
 
 // PUT - updatepattern
