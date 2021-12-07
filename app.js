98e1d760c548362c07fa7ac2085a463b98730bf4
Daniel Hoogerwerf <d.hoogerwerf@icloud.com>
2021-12-07 19:13:39 +0100

    Maintenance fixes

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-const cors       = require("cors");
-    
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const cors = require("cors");
 
-mongoose
+const initMongoose = mongoose
   .connect(process.env.MONGODB_URL, {
     // .connect("mongodb://localhost/drumgrid-backend", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then((x) => {
-    console.log(`Connected to Mongo! Host: ${x.connections[0].host}, Port: ${x.connections[0].port}, Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Host: ${x.connections[0].host}, Port: ${x.connections[0].port}, Database name: "${x.connections[0].name}"`
+    );
+    return x.connection.getClient();
   })
   .catch((err) => {
     console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://www.drumgrid.nl", "https://www.drumgrid.nl"],
+    origin: [
+      "http://localhost:3000",
+      "http://www.drumgrid.nl",
+      "https://www.drumgrid.nl",
+    ],
     credentials: true,
   })
 );
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
 
 // app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // Enable authentication using session + passport
 app.use(
@@ -63,18 +68,23 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store: MongoStore.create({
+      client: initMongoose,
+      mongoOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    }),
   })
 );
 app.use(flash());
-require('./passport')(app);
-    
+require("./passport")(app);
 
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
 
-const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
 
 const profileRoutes = require("./routes/profile");
 app.use("/api/profile", profileRoutes);
